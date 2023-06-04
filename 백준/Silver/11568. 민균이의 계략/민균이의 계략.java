befe7fa347cff75a5a245851ceed0d92735b6a4e
pH-Saem <79356675+pH-Saem@users.noreply.github.com>
2023-06-04 09:40:20 +0900

    [Silver II] Title: 민균이의 계략, Time: 96 ms, Memory: 12188 KB -BaekjoonHub

---
+++ 백준/Silver/11568. 민균이의 계략/민균이의 계략.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int N;
+    static List<Integer> lis = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+
+        int num, index;
+        for (int i = 0; i < N; i++) {
+            num = Integer.parseInt(st.nextToken());
+
+            index = Collections.binarySearch(lis, num);
+            if (index < 0) {
+                index = index * (-1) - 1;
+            }
+
+            if (index < lis.size()) {
+                lis.set(index, num);
+            } else {
+                lis.add(index, num);
+            }
+        }
+        System.out.println(lis.size());
+    }
+}
